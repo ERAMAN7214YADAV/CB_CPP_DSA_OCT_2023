5424161a83581235ea76ae09866a88c16f096377
ERAMAN7214YADAV <amanyadav081786@gmail.com>
2024-02-02 08:51:18 +0530

    first commit

---
+++ Max_Value_In_the_Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int n; 
+    cout<<"Enter the length the array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+        cout<<"Maximum value in the array is "<<max;
+    return 0;
+}
+
